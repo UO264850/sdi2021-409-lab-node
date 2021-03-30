92994a3d85d45730066492e8fdf0786bbfce2efd
UO264850 <uo264850@uniovi.es>
2021-03-30 16:37:45 +0200

    SDI-2021-409-sesión7-Ejercicio-complementario2

--- routes/rautores.js
+++ routes/rautores.js
@@ -1,7 +1,10 @@
 module.exports = function (app, swig) {
 
     app.get('/autores/agregar', function (req, res) {
-        let respuesta = swig.renderFile('views/autores-agregar.html', {});
+        let roles = ["Cantante", "Batería", "Guitarrista", "Bajista", "Teclista"];
+        let respuesta = swig.renderFile('views/autores-agregar.html', {
+            roles: roles
+        });
         res.send(respuesta);
     });
 
