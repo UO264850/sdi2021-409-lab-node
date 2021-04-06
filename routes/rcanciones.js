5aadfc1945f99ebe8dafb8bd62e5b5783c355246
UO264850 <uo264850@uniovi.es>
2021-04-06 21:52:48 +0200

    SDI-2021-409-8.4-Registro de usuario, autenticación y uso de sesión.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,5 +1,10 @@
 module.exports = function(app, swig, gestorBD) {
     app.get('/canciones/agregar', function (req, res) {
+        if ( req.session.usuario == null){
+            res.redirect("/tienda");
+            return;
+        }
+
         let respuesta = swig.renderFile('views/bagregar.html', {
 
         });
@@ -45,10 +50,16 @@ module.exports = function(app, swig, gestorBD) {
     });
 
     app.post("/cancion", function(req, res){
-       let cancion = {
+        if ( req.session.usuario == null){
+            res.redirect("/tienda");
+            return;
+        }
+
+        let cancion = {
            nombre : req.body.nombre,
            genero : req.body.genero,
-           precio : req.body.precio
+           precio : req.body.precio,
+           autor: req.session.usuario
        }
        //Conectarse
         gestorBD.insertarCancion(cancion, function(id){
