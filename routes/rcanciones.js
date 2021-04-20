988ee98c52a08f9e358d52f26064e43c86504975
UO264850 <uo264850@uniovi.es>
2021-04-21 01:40:24 +0200

    SDI-2021-409-10.5-SW Consumir SW desde NodeJS

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -139,26 +139,41 @@ module.exports = function(app, swig, gestorBD) {
                 res.send("Error al recuperar la canción.");
             } else {
                 cancionDisponible(req.session.usuario, canciones[0], cancion_id, function(enPropiedad){
-                    gestorBD.obtenerComentarios(cancion_id, function(comentariosCancion){
-                        if(comentariosCancion == null){
-                            let comentariosCancion = [];
-                            let respuesta = swig.renderFile('views/bcancion.html',
-                                {
-                                    cancion : canciones[0],
-                                    comprada: enPropiedad,
-                                    comentarios: comentariosCancion
-                                });
-                            res.send(respuesta);
-                        } else{
-                            let respuesta = swig.renderFile('views/bcancion.html',
-                                {
-                                    cancion : canciones[0],
-                                    comprada: enPropiedad,
-                                    comentarios: comentariosCancion
-                                });
-                            res.send(respuesta);
+                    let configuracion = {
+                        url: "https://www.freeforexapi.com/api/live?pairs=EURUSD",
+                        method: "get",
+                        headers: {
+                            "token": "ejemplo",
                         }
-                    });
+                    }
+                    let rest = app.get("rest");
+                    rest(configuracion, function (error, response, body) {
+                        console.log("cod: " + response.statusCode + " Cuerpo :" + body);
+                        let objetoRespuesta = JSON.parse(body);
+                        let cambioUSD = objetoRespuesta.rates.EURUSD.rate;
+                        // nuevo campo "usd"
+                        canciones[0].usd = cambioUSD * canciones[0].precio;
+                        gestorBD.obtenerComentarios(cancion_id, function(comentariosCancion){
+                            if(comentariosCancion == null){
+                                let comentariosCancion = [];
+                                let respuesta = swig.renderFile('views/bcancion.html',
+                                    {
+                                        cancion : canciones[0],
+                                        comprada: enPropiedad,
+                                        comentarios: comentariosCancion
+                                    });
+                                res.send(respuesta);
+                            } else{
+                                let respuesta = swig.renderFile('views/bcancion.html',
+                                    {
+                                        cancion : canciones[0],
+                                        comprada: enPropiedad,
+                                        comentarios: comentariosCancion
+                                    });
+                                res.send(respuesta);
+                            }
+                        });
+                    })
                 });
             }
         });
